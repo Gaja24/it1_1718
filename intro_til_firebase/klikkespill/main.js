cba45d341e358863ff62505b71b57232a6bf61ff
Gaja24 <gajenthar@hotmail.no>
2017-12-01 13:18:09 +0100

    Gaja

---
+++ intro_til_firebase/klikkespill/main.js
@@ -0,0 +1,50 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyBEMLhdt5Xf-tRAx9edcDYO2EsJ4sZRVz4",
+    authDomain: "klikkespillet24.firebaseapp.com",
+    projectId: "klikkespillet24"
+});
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
+
+
+
+var navnE = document.querySelector('.navn');
+var startE = document.querySelector('.start');
+var klikkE = document.querySelector('.klikk');
+var klikkverdiE = document.querySelector('.klikkverdi');
+var listeE = document.querySelector('.poengliste')
+
+var antKlikk = 0;
+
+
+
+klikkE.addEventListener('click', function () {
+   antKlikk = antKlikk + 1;
+   klikkverdiE.innerHTML = antKlikk;
+});
+
+startE.addEventListener('click', function () {
+    antKlikk = 0;
+    klikkverdiE.innerHTML = antKlikk;
+    setTimeout(function () {
+        db.collection("spill").add({
+         navn: navnE.value,
+         poeng: antKlikk
+    });
+        alert(antKlikk)
+    }, 4000);
+});
+
+var mappe = db.collection('spill').orderby('poeng', 'desc').limit(20);
+
+mappe.onSnapshot(function (data) {
+    var dokumenter = data.docs;
+
+    for(var x in dokumenter){
+        listeE.innerHTML += "<li>" + dokumenter[x].data().navn + ":"+ dokumenter[x].data().poeng +"</li>"
+    }
+
+
+})
